ead4a3bb98cb2a58d8f0b3a1746835018a26c93d
Lucas Suota <lucassuotass@gmail.com>
2024-03-19 16:42:31 -0300

    feat: hero added

--- components/application/components/home/Header.tsx
+++ components/application/components/home/Header.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="w-full">
+    <header className="fixed w-full bg-white">
       <div className="max-w-screen-lg mx-auto py-4 px-8 flex flex-row items-center justify-between">
         <div className="relative sm:w-[100px] sm:h-[50px] w-[70px] h-[40px]">
           <Image src={"/svgs/petito-logo.svg"} fill alt="logo" />
@@ -12,7 +12,7 @@ const Header = () => {
         <div>
           <ul className="flex flex-row items-center justify-end sm:gap-10 gap-4">
             <Link href={"/login"}>
-              <li className="bg-primaryblue px-4 py-2 sm:px-6 sm:py-4 rounded-sm hover:bg-primarypurple active:bg-primaryblack text-white cursor-pointer transition-all">
+              <li className="bg-primaryblue px-4 py-2 sm:px-6 sm:py-4 rounded-sm hover:bg-secondaryblue active:bg-black text-white cursor-pointer transition-all">
                 Entrar
               </li>
             </Link>
--- components/layout/Hero.tsx
+++ components/layout/Hero.tsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Hero = () => {
-  return <section className="w-full h-dvh ">Hero</section>;
+  return (
+    <section
+      className="w-full h-dvh flex flex-col items-center justify-center"
+      style={{
+        backgroundImage:
+          'linear-gradient(to bottom, rgba(0, 0, 0, 0.72), rgba(0, 0, 0, 0.8)), url("/images/login-background.jpg")',
+      }}
+    >
+      <p className="sm:text-7xl text-4xl font-semibold text-white sm:w-2/5 w-4/5 text-start">
+        Uma melhor forma de cuidar do seu{" "}
+        <strong className="text-primaryblue font-semibold">bichinho.</strong>
+      </p>
+    </section>
+  );
 };
 
 export default Hero;
